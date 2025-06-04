c1609f5160ba5e2919bdaed3747936711c617007
Rémi Héneault <remi@heneault.fr>
2025-06-04 16:49:34 +0100

    Filter out health checks in logs

--- webapp.py
+++ webapp.py
@@ -1,12 +1,20 @@
+import logging
 from pathlib import Path
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
+from uvicorn.logging import DefaultFormatter
 
 from views import http_router, ws_router
 
 
 app = FastAPI()
+
+class HealthCheckFilter(logging.Filter):
+    def filter(self, record):
+        return '"GET /health HTTP/1.1" 200' not in record.getMessage()
+
+logging.getLogger("uvicorn.access").addFilter(HealthCheckFilter())
 
 @app.get("/health")
 async def get_health():
